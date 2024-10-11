0e2f6a35f6d735607ba51d4d848dbdc320c81a11
Moddy-t <amalembamodesta@gmail.com>
2024-10-11 11:49:15 +0300

    cookie sessions lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,24 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+   session['page_views'] = session.get('page_views', 0) + 1
+   if session['page_views'] > 3:
+       return {'message': 'Maximum pageview limit reached'}, 401
+   
+   articles = Article.query.all()
+   article_dicts =[article.to_dict() for article in articles]
+   return jsonify(article_dicts), 200
+    
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    session['page_views'] = session.get('page_views', 0) + 1
+    if session['page_views'] > 3:
+        return {'message': 'Maximum pageview limit reached'}, 401
+    article = Article.query.filter(Article.id == id).first()
+    if not article:
+        return {'message': 'Article not found'}, 404
+    return jsonify(article.to_dict()), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
